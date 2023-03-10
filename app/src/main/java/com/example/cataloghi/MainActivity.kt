a242af9dcbab0e970a5f46268bb94a9081a0c8be
telmajohanns <tej2@hi.is>
2023-03-10 21:48:24 +0000

    First commit

---
+++ app/src/main/java/com/example/cataloghi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cataloghi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    supportFragmentManager.beginTransaction().add(R.id.container,LoginFragment()).commit()
+  }
+}
